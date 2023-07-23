f154df04b4ad8528ee34d33f9e2f80d0e591ab9f
ewanche <134198582+ewanche@users.noreply.github.com>
2023-07-23 21:46:40 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,15 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Electricer in Germany experience Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Ivan Che`,
+      summary: `who lives and works in Steinfurt building electricity.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      telegram: `@evan_che`,
     },
   },
   plugins: [
